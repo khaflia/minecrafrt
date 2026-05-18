f49e1ff505703c708effecf24fe8b2da4eb9fb64
Kbk <75666989+khaflia@users.noreply.github.com>
2026-05-18 18:48:00 +0400

    Update tablist footer to show Discord invite links

--- src/main/java/me/kbk/dhabicore/scoreboard/ScoreboardManager.java
+++ src/main/java/me/kbk/dhabicore/scoreboard/ScoreboardManager.java
@@ -121,7 +121,7 @@ public class ScoreboardManager {
         player.setCustomNameVisible(true);
         player.setPlayerListHeaderFooter(
             c("&b&lKhaleeji SMP\n&fplay.khaleeji.lol"),
-            c("\n&fDiscord: &b/gnty &7| &b/dhabi")
+            c("\n&fDiscord: &bdiscord.gg/dhabi &7| &bdiscord.gg/gnty")
         );
     }
 
